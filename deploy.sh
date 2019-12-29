ae0be4d81698f323a193d842ed5a349b2478075a
张芳朝 <17602143402@163.com>
2019-12-29 15:08:23 +0800

    let self = this

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ echo 'zhangfangchao.com' > CNAME
 
 git init
 git add .
-git commit -m 'webpack 优化'
+git commit -m 'let self = this'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
