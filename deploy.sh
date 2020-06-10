24b9039e4c309d232645b00f0385f0c586ac54a2
张芳朝 <17602143402@163.com>
2020-06-10 09:40:29 +0800

    feat：修改commit

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ echo 'zhangfangchao.com' > CNAME
 
 git init
 git add .
-git commit -m 'webpack 抽离第三方模块'
+git commit -m '新增 linux 模块'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
