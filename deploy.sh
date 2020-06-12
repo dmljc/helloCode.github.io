1e924e8f6a698f0aeed5f3599c424ea1e6c95e52
张芳朝 <17602143402@163.com>
2020-06-12 09:47:37 +0800

    feat: git add tag

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ echo 'zhangfangchao.com' > CNAME
 
 git init
 git add .
-git commit -m 'linux 模块新增备份压缩相关知识点'
+git commit -m 'git add tag'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
